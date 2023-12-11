b79989cd7f1decc13237dacbfa78afab91f37733
¨danmoren¨ <morenoacedo@gmail.com>
2023-12-11 14:40:25 +0100

    funcion ft_isdigit 11/12/23

--- libft/ft_isdigit.c
+++ libft/ft_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: danmoren <danmoren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 12:28:09 by danmoren          #+#    #+#             */
-/*   Updated: 2023/12/11 13:57:15 by danmoren         ###   ########.fr       */
+/*   Updated: 2023/12/11 14:39:21 by danmoren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,16 @@ int	ft_isdigit(int c)
 	else
 		return (0);
 }
-#include <ctype.h>
-#include <studio.h>
+/* #include <ctype.h>
+#include <stdio.h>
 
+int main()
+{
+	int c = 'q';
+	int b = '5';
+
+	printf("ft_isdigit: %d\n", ft_isdigit(c));
+	printf("isdigit: %d\n", isdigit(c));
+	printf("ft_isdigit: %d\n", ft_isdigit(b));
+	printf("isdigit: %d\n", isdigit(b));
+} */
